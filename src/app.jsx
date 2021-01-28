2b04fd6eded0775eeba6727b50eb04fefae0fa15
RoyalOttoc <jong.kim8706@gmail.com>
2021-01-29 05:17:13 +1100

    component: Habit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './app.css';
+import Habit from './components/habit';
+
+
+
+class App extends Component {
+  render() {
+    return <Habit />;
+  }
+}
+
+export default App;
