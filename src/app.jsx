2a41c00dd1e426ba95af0e68a5dafec4a1fc6d0a
RoyalOttoc <jong.kim8706@gmail.com>
2021-01-29 17:41:37 +1100

    habits: plus,minus,delete

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './app.css';
-import Habit from './components/habit';
+import Habits from './components/habits';
 
 
 
 class App extends Component {
   render() {
-    return <Habit />;
+    return <Habits />;
   }
 }
 
