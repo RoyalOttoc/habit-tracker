9a9b51b3722f157abea199e4e221670235d3ebec
RoyalOttoc <jong.kim8706@gmail.com>
2021-01-29 18:32:25 +1100

    jsx: navbar

---
+++ src/components/add.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Add extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="Habit" />
+        <button>Add</button>
+      </div>
+    );
+  }
+}
+
+export default Add;
